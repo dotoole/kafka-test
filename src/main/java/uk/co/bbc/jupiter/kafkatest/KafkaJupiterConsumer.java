b607a1835978b8575ca1482ab09373d2fc34ac3b
Naveen Manga <naveen.manga02.ext@bbc.co.uk>
2019-01-10 16:58:46 +0000

    Work in progress

--- src/main/java/uk/co/bbc/jupiter/kafkatest/KafkaJupiterConsumer.java
+++ src/main/java/uk/co/bbc/jupiter/kafkatest/KafkaJupiterConsumer.java
@@ -1,24 +1,29 @@
 package uk.co.bbc.jupiter.kafkatest;
 
+import com.google.common.collect.Lists;
+import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@Configuration
+@Service
 @EnableKafka
-public class KafkaConsumerConfig {
-        private static Logger logger = LoggerFactory.getLogger(KafkaConsumerConfig.class);
+public class KafkaJupiterConsumer {
+        private static Logger logger = LoggerFactory.getLogger(KafkaJupiterConsumer.class);
 
         @Value("${spring.kafka.bootstrap-servers}")
         private String bootstrapServer;
@@ -31,16 +36,24 @@ public class KafkaConsumerConfig {
         @Value("${ssl.keystore.location}")
         private String keyStoreLocation;
 
+        @Value(("${jupiter.kafka.topic}"))
+        private String kafkaTopic;
 
-        @Bean
-        public ConsumerFactory<String, String> consumerFactory() {
+
+        @KafkaListener(topics = "${spring.kafka.topic.produce.to}", groupId = "${jupiter.kafka.consumer.group-id}")
+        public void listen(final ConsumerRecord<?, ?> cr) throws Exception {
+                logger.info("Message recieved! - {}", cr.value());
+        }
+
+        @Scheduled(fixedDelay = 6000)
+        public void readMessages() {
                 Map<String, Object> props = new HashMap<>();
                 props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
                 props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
                 props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
                 props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
                 props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
-                props.put(ConsumerConfig.CLIENT_ID_CONFIG, "123");
+                props.put(ConsumerConfig.CLIENT_ID_CONFIG, "naveen-local-machine");
                 props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
                 props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, 60000);
                 props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, 60000);
@@ -49,23 +62,26 @@ public class KafkaConsumerConfig {
                 props.put("security.protocol", "SSL");
                 props.put("ssl.truststore.location", trustStoreLocation);
                 props.put("ssl.truststore.password", "confluent");
+
                 props.put("ssl.key.password", "confluent");
                 props.put("ssl.keystore.password", "confluent");
                 props.put("ssl.keystore.location", keyStoreLocation);
 
-                return new DefaultKafkaConsumerFactory<>(props);
+                ConsumerFactory<Integer, String> cf = new DefaultKafkaConsumerFactory<>(props);
+                Consumer<Integer, String> consumer = cf.createConsumer();
+
+                //KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+                try {
+                     //consumer.subscribe(Arrays.asList(kafkaTopic));
+                     consumer.assign(Lists.newArrayList(new TopicPartition(kafkaTopic, 0)));
+                        ConsumerRecords<Integer, String> records = consumer.poll(100);
+                        System.err.println("records size=>"+records.count());
+                     for (ConsumerRecord<Integer, String> record : records)
+                            System.out.printf("offset = %d, key = %s, value = %s%n", record.offset(), record.key(), record.value());
+                } catch (Exception ex){
+                   ex.printStackTrace();
+                } finally {
+                   consumer.close();
+                }
         }
-
-        @Bean
-        public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
-                ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
-                factory.setConsumerFactory(consumerFactory());
-                return factory;
-        }
-
-
-//        @KafkaListener(topics = "${spring.kafka.topic.produce.to}", groupId = "${jupiter.kafka.consumer.group-id}")
-//        public void listen(@Payload Message message) throws Exception {
-//                logger.info("Message recieved! - {}", message);
-//        }
 }
--- src/main/java/uk/co/bbc/jupiter/kafkatest/KafkaTestApplication.java
+++ src/main/java/uk/co/bbc/jupiter/kafkatest/KafkaTestApplication.java
@@ -1,30 +1,17 @@
 package uk.co.bbc.jupiter.kafkatest;
 
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableKafka
-//@EnableDiscoveryClient
+@EnableScheduling
+@EnableDiscoveryClient
 public class KafkaTestApplication {
-
-    private static Logger logger = LoggerFactory.getLogger(KafkaTestApplication.class);
-
-    @Value("${spring.kafka.bootstrap-servers}")
-    private String bootstrapServers;
-
-    @Value("${ssl.truststore.location}")
-    private String trustStoreLocation;
-
-    @Value("${ssl.keystore.location}")
-    private String keyStoreLocation;
 
     @Autowired
     public KafkaTestApplication() {
@@ -35,8 +22,5 @@ public class KafkaTestApplication {
         SpringApplication.run(KafkaTestApplication.class, args);
     }
 
-    @KafkaListener(topics = "test-naveen", groupId = "test-consumer-group")
-    public void listen(final ConsumerRecord<?, ?> cr) throws Exception {
-        logger.info("Message recieved! - {}", cr.value());
-    }
+
 }
