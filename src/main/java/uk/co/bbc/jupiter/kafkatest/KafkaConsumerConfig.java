3d909810f0206b2fc6b655297175985b9ad9621c
naveenkvm <naveenkvm@gmail.com>
2019-01-11 14:04:42 +0000

    Removed duplicate group id on consumer config

--- src/main/java/uk/co/bbc/jupiter/kafkatest/KafkaConsumerConfig.java
+++ src/main/java/uk/co/bbc/jupiter/kafkatest/KafkaConsumerConfig.java
@@ -42,7 +42,6 @@ public class KafkaConsumerConfig {
                 props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
                 props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
                 props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-                props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
                 props.put(ConsumerConfig.CLIENT_ID_CONFIG, "123");
                 props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
                 props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, 60000);
